5f760cfd983315823fda14a6e6b7febe55d7ed76
Betrax <oguzalinrb@gmail.com>
2020-11-01 22:45:14 +0100

    2.0 - Creates directory if it doesn't exists, searches directly for a character in file name

--- file mover.py
+++ file mover.py
@@ -1,16 +1,20 @@
 import glob
 import shutil
+import os
 
-Dir = "D:/Dir/"  # Where are the files?
-TragetDir = "D:/TargetDir/"  # Where are the target folders?
+Dir = "D:/dir"  # Where are the files?
+TragetDir = "D:/target/dir"  # Where are the target folders?
+
+FileNames = glob.glob(Dir + "*-*")  # scans the dir with the selected character (*[selected character]*), searches directly for the name now.
 
 StartIndex = len(Dir)  # The chars of the Dir are useless
-
-FileNames = glob.glob(Dir + "*.docx")  # Scans the Dir for files with the slected extension
-
-
 for x in range(len(FileNames)):  # Each file gets moved one by one
-    Index = FileNames[x].find("-")  # I always create my files using: class + "-" + topic
+    Index = FileNames[x].find("-")  # I always create my files using: "class + "-" + topic" way
     SFolder = FileNames[x][StartIndex:Index]  # My folders are always named after the subject, so "Math- exam 1.docx" should be placed in a folder named "Math"
 
+    try: #tries to make a target dir
+        os.makedirs(TragetDir + SFolder)
+    except: #if the directory already exists just skips this process
+        pass
+    
     shutil.move(FileNames[x], TragetDir + SFolder)  # moves to the target folder
