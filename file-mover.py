6ce7ca052db00ce52c3873f577f8bb2d217057bb
Betrax <oguz-nrb@hotmail.com>
2020-12-23 12:42:05 +0100

    nothing

--- file-mover.py
+++ file-mover.py
@@ -12,9 +12,9 @@ for x in range(len(FileNames)):  # Each file gets moved one by one
     Index = FileNames[x].find("-")  # I always create my files using: "class + "-" + topic" way
     SFolder = FileNames[x][StartIndex:Index]  # My folders are always named after the subject, so "Math- exam 1.docx" should be placed in a folder named "Math"
 
-    try: #tries to make a target dir
+    try:  # tries to make a target dir
         os.makedirs(TragetDir + SFolder)
-    except: #if the directory already exists just skips this process
+    except:  # if the directory already exists just skips this process
         pass
-    
+
     shutil.move(FileNames[x], TragetDir + SFolder)  # moves to the target folder
